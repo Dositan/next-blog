68a58759f779b64bbe0002febf3dd3adc6127020
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-06-27 18:50:04 +0600

    ✅ Change homepage title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Dastan Ozgeldi";
-export const siteTitle = `${name} | Homepage`;
+export const siteTitle = `Home | ${name}`;
 
 export default function Layout({ children, home }) {
   return (
